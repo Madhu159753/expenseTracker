35fe2959e358997fe2f668a33a7d0eecfc4ab466
Madhu159753 <116351674+Madhu159753@users.noreply.github.com>
2023-02-10 06:32:25 +0530

    Update database.js
    done

--- util/database.js
+++ util/database.js
@@ -2,8 +2,8 @@ const dotenv=require('dotenv')
 const Sequelize=require('sequelize');
 dotenv.config();
 const sequelize=new Sequelize(process.env.MYSQL_SCHEMA,process.env.MYSQL_USER,process.env.MYSQL_PASSWORD,{
-    dialect:'mysql'
-    host:process.env.MYSQL_HOS
+    dialect:'mysql',
+    host:process.env.MYSQL_HOST
 });
 
 module.exports=sequelize;
